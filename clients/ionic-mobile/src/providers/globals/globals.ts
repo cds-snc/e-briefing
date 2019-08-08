e0597fe17486639119d6d4367d833a08931bac72
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-08-08 11:41:52 -0400

    update api_url to local server (should move out to env var)

--- clients/ionic-mobile/src/providers/globals/globals.ts
+++ clients/ionic-mobile/src/providers/globals/globals.ts
@@ -11,7 +11,7 @@ export class GlobalsProvider {
     dataDirectory: string = '';
     api_key: string = 'API_KEY';
     trip_id: any = 'TRIP_ID';
-    api_url: string = 'https://ebrief.cds-snc.ca/api';
+    api_url: string = 'http://localhost:8080/api';
 
     constructor(public http: Http, private platform: Platform, private file: File) {
         platform.ready().then(() => {
