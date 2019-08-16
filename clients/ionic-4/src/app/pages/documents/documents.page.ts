0f3a97ec08e97886aaa122c01919cd7b8fe8ea58
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-08-16 16:49:24 -0400

    add notes and documents

--- clients/ionic-4/src/app/pages/documents/documents.page.ts
+++ clients/ionic-4/src/app/pages/documents/documents.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-documents',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentsPage implements OnInit {
 
+  documents: any;
+  keys: string[];
+
   constructor() { }
 
   ngOnInit() {
+    fetch(environment.data_directory + '/documents.json')
+      .then(res => res.json())
+      .then(json => {
+        this.documents = json;
+        this.keys = Object.keys(this.documents);
+      });
   }
 
 }
--- clients/ionic-4/src/app/pages/notes/notes.page.ts
+++ clients/ionic-4/src/app/pages/notes/notes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-notes',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotesPage implements OnInit {
 
+  notes: any;
+
   constructor() { }
 
   ngOnInit() {
+    fetch(environment.data_directory + '/articles.json')
+      .then(res => res.json())
+      .then(json => {
+        this.notes = json;
+      });
   }
 
 }
