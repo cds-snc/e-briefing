c1ddcc2a4d15508281dfd15122464ef83877e0d6
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-08-16 15:52:42 -0400

    use header module

--- clients/ionic-4/src/app/pages/contacts/contacts.module.ts
+++ clients/ionic-4/src/app/pages/contacts/contacts.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { ContactsPage } from './contacts.page';
+import { ComponentsModule } from '../../components/components.module';
 
 const routes: Routes = [
   {
@@ -19,8 +20,9 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+    ComponentsModule,
     RouterModule.forChild(routes)
   ],
   declarations: [ContactsPage]
 })
-export class ContactsPageModule {}
+export class ContactsPageModule { }
