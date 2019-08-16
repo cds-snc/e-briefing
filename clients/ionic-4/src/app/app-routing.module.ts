750226804cf4b9d73e63af1e6a0bbf1248d2e206
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-08-16 10:34:25 -0400

    group pages into a folder

--- clients/ionic-4/src/app/app-routing.module.ts
+++ clients/ionic-4/src/app/app-routing.module.ts
@@ -9,21 +9,17 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
   },
-  {
-    path: 'list',
-    loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
-  },
-  { path: 'note', loadChildren: './note/note.module#NotePageModule' },
-  { path: 'notes', loadChildren: './notes/notes.module#NotesPageModule' },
-  { path: 'contact', loadChildren: './contact/contact.module#ContactPageModule' },
-  { path: 'contacts', loadChildren: './contacts/contacts.module#ContactsPageModule' },
-  { path: 'day', loadChildren: './day/day.module#DayPageModule' },
-  { path: 'days', loadChildren: './days/days.module#DaysPageModule' },
-  { path: 'documents', loadChildren: './documents/documents.module#DocumentsPageModule' },
-  { path: 'event', loadChildren: './event/event.module#EventPageModule' },
-  { path: 'sync', loadChildren: './sync/sync.module#SyncPageModule' }
+  { path: 'note', loadChildren: './pages/note/note.module#NotePageModule' },
+  { path: 'notes', loadChildren: './pages/notes/notes.module#NotesPageModule' },
+  { path: 'contact', loadChildren: './pages/contact/contact.module#ContactPageModule' },
+  { path: 'contacts', loadChildren: './pages/contacts/contacts.module#ContactsPageModule' },
+  { path: 'day', loadChildren: './pages/day/day.module#DayPageModule' },
+  { path: 'days', loadChildren: './pages/days/days.module#DaysPageModule' },
+  { path: 'documents', loadChildren: './pages/documents/documents.module#DocumentsPageModule' },
+  { path: 'event', loadChildren: './pages/event/event.module#EventPageModule' },
+  { path: 'sync', loadChildren: './pages/sync/sync.module#SyncPageModule' }
 ];
 
 @NgModule({
