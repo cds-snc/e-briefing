30d3a97dc19c899042e43d232d44daa9b54f54fb
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-08-16 16:35:04 -0400

    load days

--- clients/ionic-4/src/app/pages/days/days.page.ts
+++ clients/ionic-4/src/app/pages/days/days.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-days',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DaysPage implements OnInit {
 
+  days: any;
+  binder: any;
+
   constructor() { }
 
   ngOnInit() {
+    fetch(environment.data_directory + '/days.json')
+      .then(res => res.json())
+      .then(json => {
+        this.days = json;
+      });
+
+    fetch(environment.data_directory + '/binder.json')
+      .then(res => res.json())
+      .then(json => {
+        this.binder = json;
+      });
   }
 
 }
