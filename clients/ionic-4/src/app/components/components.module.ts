ece9c587cd88b2c461890f09611c76af8f2f4b1d
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-08-19 14:18:44 -0400

    add module

--- clients/ionic-4/src/app/components/components.module.ts
+++ clients/ionic-4/src/app/components/components.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from '../components/header/header.component';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
     declarations: [
         HeaderComponent
+    ],
+    imports: [
+        IonicModule
     ],
     exports: [
         HeaderComponent
