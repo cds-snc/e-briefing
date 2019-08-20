c4a87dbffd48b161acf7dff789c30a344b524b36
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-08-20 15:11:00 -0400

    add footer component

--- clients/ionic-4/src/app/components/components.module.ts
+++ clients/ionic-4/src/app/components/components.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from '../components/header/header.component';
 import { ItineraryComponent } from '../components/itinerary/itinerary.component';
+import { FooterComponent } from '../components/footer/footer.component';
 
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
@@ -8,6 +9,7 @@ import { CommonModule } from '@angular/common';
 @NgModule({
     declarations: [
         HeaderComponent,
+        FooterComponent,
         ItineraryComponent
     ],
     imports: [
@@ -16,6 +18,7 @@ import { CommonModule } from '@angular/common';
     ],
     exports: [
         HeaderComponent,
+        FooterComponent,
         ItineraryComponent
     ]
 })
