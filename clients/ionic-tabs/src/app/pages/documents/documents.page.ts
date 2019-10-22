bc6f294f534e7410f8d1d060acb5080e6639f15c
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-10-21 20:27:04 -0400

    add documentViewer

--- clients/ionic-tabs/src/app/pages/documents/documents.page.ts
+++ clients/ionic-tabs/src/app/pages/documents/documents.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from '../../../environments/environment';
+import { GlobalsService } from 'src/app/services/globals.service';
+import { Platform } from '@ionic/angular';
+import { File } from '@ionic-native/file/ngx';
+import { HttpClient } from '@angular/common/http';
+import { DocumentViewer } from '@ionic-native/document-viewer/ngx';
 
 @Component({
   selector: 'app-documents',
@@ -9,17 +14,38 @@ import { environment } from '../../../environments/environment';
 export class DocumentsPage implements OnInit {
 
   documents: any;
+  id: any;
+  selectedDocument: any;
   keys: string[];
 
-  constructor() { }
+  constructor(private globals: GlobalsService, private platform: Platform, private file: File, private document: DocumentViewer) { }
 
   ngOnInit() {
-    fetch(environment.data_directory + '/documents.json')
-      .then(res => res.json())
-      .then(json => {
-        this.documents = json;
-        this.keys = Object.keys(this.documents);
-      });
+
   }
 
+  showDocument(id) {
+    const options = {
+      title: 'PDF'
+    };
+
+    this.file.readAsText(this.globals.dataDirectory + 'documents/', id + '.json').then(res => {
+      this.selectedDocument = JSON.parse(res);
+      this.document.viewDocument(this.globals.dataDirectory + 'assets/' + this.selectedDocument.file, 'application/pdf', options);
+    });
+  }
+
+  async ionViewWillEnter() {
+
+    await this.platform.ready();
+
+    await this.file.checkFile(this.globals.dataDirectory, 'documents.json')
+      .then(_ => console.log('Documents.json exists'))
+      .catch(err => console.log(err));
+
+    this.file.readAsText(this.globals.dataDirectory, 'documents.json').then(res => {
+      this.documents = JSON.parse(res);
+      this.keys = Object.keys(this.documents);
+    });
+  }
 }
