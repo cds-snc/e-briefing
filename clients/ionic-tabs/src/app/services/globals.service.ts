0249c4817128fc8bc0b3ceddd6c3ce3298ec61e4
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-10-21 20:52:25 -0400

    add services

---
+++ clients/ionic-tabs/src/app/services/globals.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { File } from '@ionic-native/file/ngx';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GlobalsService {
+
+  dataDirectory = './assets/data';
+
+  constructor(private platform: Platform, private file: File) {
+    platform.ready().then(() => {
+      console.log('platform ready');
+      // if (this.platform.is('ios')) {
+      if (this.file.dataDirectory) {
+        this.setDataDirectory(this.file.dataDirectory);
+      }
+    });
+  }
+
+  setDataDirectory(value) {
+    this.dataDirectory = value;
+  }
+
+  getDataDirectory() {
+    return this.dataDirectory;
+  }
+
+}
