9a0b3236337ce1924dcfc5d6452b058f6945ceae
Tim Arney <tim@line37.com>
2019-10-29 07:14:55 -0400

    cleanup + contact page

--- clients/ionic-tabs/src/app/pages/contacts/contacts.page.ts
+++ clients/ionic-tabs/src/app/pages/contacts/contacts.page.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { Platform } from '@ionic/angular';
-import { GlobalsService } from 'src/app/services/globals.service';
-import { File } from '@ionic-native/file/ngx';
-import { WebView } from '@ionic-native/ionic-webview/ngx';
-import { DomSanitizer } from '@angular/platform-browser';
+import { Component, OnInit } from "@angular/core";
+import { Platform } from "@ionic/angular";
+import { GlobalsService } from "src/app/services/globals.service";
+import { File } from "@ionic-native/file/ngx";
+import { WebView } from "@ionic-native/ionic-webview/ngx";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-contacts',
-  templateUrl: './contacts.page.html',
-  styleUrls: ['./contacts.page.scss'],
+  selector: "app-contacts",
+  templateUrl: "./contacts.page.html",
+  styleUrls: ["./contacts.page.scss"]
 })
 export class ContactsPage implements OnInit {
   contacts: any;
@@ -20,32 +20,39 @@ export class ContactsPage implements OnInit {
     private file: File,
     private webview: WebView,
     private sanitizer: DomSanitizer
-  ) { }
+  ) {}
 
-  ngOnInit() { }
+  ngOnInit() {}
 
   async ionViewWillEnter() {
     await this.platform.ready();
 
-    this.imgPath = this.webview.convertFileSrc(this.globals.dataDirectory + 'assets/');
+    this.imgPath = this.webview.convertFileSrc(this.globals.dataDirectory);
 
-    await this.file.checkFile(this.globals.dataDirectory, 'people.json')
-      .then(_ => console.log('People.json exists'))
+    await this.file
+      .checkFile(this.globals.dataDirectory, "people.json")
+      .then(_ => console.log("People.json exists"))
       .catch(err => console.log(err));
 
-    this.file.readAsText(this.globals.dataDirectory, 'people.json').then(res => {
-      this.contacts = JSON.parse(res);
-    });
+    this.file
+      .readAsText(this.globals.dataDirectory, "people.json")
+      .then(res => {
+        this.contacts = JSON.parse(res);
+      });
   }
 
   pathForImage(img) {
     if (img === null) {
-      return '';
+      return "";
     } else {
-      const converted = this.webview.convertFileSrc(this.globals.dataDirectory + 'assets/' + img);
+      const arr = img.split("/");
+      const fileName = arr[arr.length - 1];
+
+      const converted = this.webview.convertFileSrc(
+        `${this.globals.dataDirectory}/${fileName}`
+      );
       const safeImg = this.sanitizer.bypassSecurityTrustUrl(converted);
       return safeImg;
     }
   }
-
 }
